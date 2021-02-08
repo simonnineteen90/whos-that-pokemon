afcd000548d768193c0c9b4f518fe7996ae3980d
Simon Dunn <simondunn90@gmail.com>
2021-02-08 20:24:00 +0000

    created check_answer route and recieved user guess from form via POST method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pokebase as pb
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -12,6 +12,9 @@ app = Flask(__name__)
 #     image = pb.SpriteResource('pokemon', id)
 #     return f"No.{id_num} Name: {name} Height: {height}, Image path: {image.path}"
 
+## use a decorator function to generate the poke data?
+# cant work out how to access the pokemon name to check if the user guess is correct
+
 
 @app.route('/')
 def random_pokemon():
@@ -19,9 +22,15 @@ def random_pokemon():
     name = pb.pokemon(id_num)
     height = name.height
     image = pb.SpriteResource('pokemon', id_num)
-    print(name)
     return render_template('index.html', p_num=id_num, p_name=name ,p_height=height, p_image=image.url)
 
+
+
+@app.route('/check_answer', methods=["POST"])
+def check_answer():
+    user_answer = request.form["guess"]
+
+    return f"Check answer page. User answer = {user_answer}"
 
 
 if __name__ == "__main__":
