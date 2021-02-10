6c646eeb9941c2df9903e80130a84c9617819619
Simon Dunn <simondunn90@gmail.com>
2021-02-10 16:30:29 +0000

    created additional routes. Logic to keep track of which pokemon already chosen

--- main.py
+++ main.py
@@ -7,23 +7,39 @@ app = Flask(__name__)
 # require a secret key to use session variable
 app.secret_key = "secretPokemon"
 
+
 @app.route('/')
+def start():
+    session['chosen_ids'] = []
+    return render_template('index.html')
+
+
+@app.route('/random_pokemon')
 def random_pokemon():
     """
     Returns a random pokemon from pokebase / the API
     :return:
     """
-
+    #session['chosen_ids'] = []
     id_num = random.randint(1, 151)
+
+    # while the id_num is in the chosen_ids list generate another random number and check that number
+    # to stop duplicate pokemon coming up
+    while id_num in session['chosen_ids']:
+        id_num = random.randint(1, 151)
+
+    #if session['chosen_ids'] length is > 151 then the game is completed.
+
+    session['chosen_ids'].append(id_num)
+    print(session['chosen_ids'])
     pokemon = pb.pokemon(id_num)
     image = pb.SpriteResource('pokemon', id_num)
     height = pokemon.height
     session['pokemon_name'] = str(pokemon)
     print(f"Session variable = {session['pokemon_name']}")
 
-    return render_template('index.html', p_num=id_num, p_name=pokemon,
+    return render_template('random_pokemon.html', p_num=id_num, p_name=pokemon,
                            p_height=height, p_image=image.url)
-
 
 
 @app.route('/check_answer', methods=["POST"])
@@ -35,9 +51,9 @@ def check_answer():
     """
     user_answer = request.form["guess"]
     if user_answer != session['pokemon_name']:
-        return f" Incorrect answer. User answer = {user_answer}"
+        return render_template('incorrect.html')
     elif user_answer == session['pokemon_name']:
-        return f"correct answer! User answer = {user_answer}"
+        return render_template('correct.html')
     else:
         return redirect(url_for('random_pokemon'))
 
@@ -53,4 +69,9 @@ if __name__ == "__main__":
 # get the user answer
 # check user answer
 # display page based on correct/incorrect answer
-# update the user score
+# if correct answer
+#   update the user score
+#   load next pokemon or quit
+# elif incorrect answer
+#   reset user score
+#   reset pokemon list to empty
